5cca9743db07fbd3845141874d9dbb6fdb277ab8
Cristian Sulea <cristian.sulea.79@gmail.com>
2014-07-29 17:09:42 +0300

    description updated

--- jatoo-resources/src/main/java/jatoo/resources/package-info.java
+++ jatoo-resources/src/main/java/jatoo/resources/package-info.java
@@ -1,8 +1,7 @@
 /**
- * A Java Open Source library created to ease the work with various resources (like locale specific texts, images, icons, etc).
+ * A free open source Java library created to ease the work with various resources (like locale specific texts, images, icons, etc).
  * 
  * @author <a href="http://cristian.sulea.net" rel="author">Cristian Sulea</a>
- * @version 1.0, July 2, 2014
  */
 package jatoo.resources;
 
