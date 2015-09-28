1671bb53333acc6098ea80507074efa68e88ee69
Cristian Sulea <cristian.sulea.79@gmail.com>
2015-09-28 15:18:27 +0300

    better tests

--- jatoo-resources/src/test/java/jatoo/resources/ResourcesTextsTest.java
+++ jatoo-resources/src/test/java/jatoo/resources/ResourcesTextsTest.java
@@ -2,6 +2,7 @@ package jatoo.resources;
 
 import java.util.Locale;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class ResourcesTextsTest {
@@ -11,25 +12,52 @@ public class ResourcesTextsTest {
 
     ResourcesTexts texts = new ResourcesTexts(getClass());
 
-    System.out.println(texts.getText("title"));
+    Assert.assertEquals("Application Title", texts.getText("title"));
+    Assert.assertEquals("Application Description", texts.getText("description"));
   }
 
   @Test
-  public void testWithDifferentLocale() {
+  public void testWithLocale1() {
+
+    Locale.setDefault(new Locale("ro"));
 
     ResourcesTexts texts = new ResourcesTexts(getClass());
 
-    Locale.setDefault(new Locale("ro"));
-    System.out.println(texts.getText("title"));
+    Assert.assertEquals("Titlul applicatiei", texts.getText("title"));
+    Assert.assertEquals("Descrierea applicatiei", texts.getText("description"));
+  }
+
+  @Test
+  public void testWithLocale2() {
+
+    Locale.setDefault(new Locale("it"));
+
+    ResourcesTexts texts = new ResourcesTexts(getClass());
+
+    Assert.assertEquals("Application Title (it)", texts.getText("title"));
+    Assert.assertEquals("Application Description (it)", texts.getText("description"));
   }
 
   @Test
   public void testWithMissingLocale() {
 
+    Locale.setDefault(new Locale("xx"));
+
     ResourcesTexts texts = new ResourcesTexts(getClass());
 
-    Locale.setDefault(new Locale("en"));
-    System.out.println(texts.getText("title"));
+    Assert.assertEquals("Application Title", texts.getText("title"));
+    Assert.assertEquals("Application Description", texts.getText("description"));
+  }
+
+  @Test
+  public void testWithMissingKey() {
+
+    Locale.setDefault(new Locale("xx"));
+
+    ResourcesTexts texts = new ResourcesTexts(getClass());
+
+    Assert.assertEquals("#yy", texts.getText("yy"));
+    Assert.assertEquals("#zz", texts.getText("zz"));
   }
 
 }
